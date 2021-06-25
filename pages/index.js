beb5b221803ba32e582d7853e852d67b8d735164
Jordan <jordanashment@gmail.com>
2021-06-24 19:55:29 -0600

    Add about page

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="/about" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
